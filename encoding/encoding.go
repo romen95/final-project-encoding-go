57dc229eabd3c12039e96afb91d926c4eda90cdc
Roman Retinskiy <romanretinskiy@yandex.ru>
2023-12-05 09:06:10 +0300

    Start a task

--- encoding/encoding.go
+++ encoding/encoding.go
@@ -1,7 +1,12 @@
 package encoding
 
 import (
+	"encoding/json"
+	"fmt"
+	"os"
+
 	"github.com/Yandex-Practicum/final-project-encoding-go/models"
+	"gopkg.in/yaml.v3"
 )
 
 // JSONData тип для перекодирования из JSON в YAML
@@ -25,16 +30,64 @@ type MyEncoder interface {
 
 // Encoding перекодирует файл из JSON в YAML
 func (j *JSONData) Encoding() error {
-	// ниже реализуйте метод
-	// ...
+	var yamlData YAMLData
+
+	// создаем файл yamlOutput.yml
+	f, err := os.Create("yamlOutput.yml")
+	if err != nil {
+		fmt.Printf("ошибка при создании файла: %s", err.Error())
+		return err
+	}
+
+	// когда программа завершится, надо закрыть дескриптор файла
+	defer f.Close()
+
+	// читаем файл JSON
+	jsonFile, err := os.ReadFile(j.FileInput)
+	if err != nil {
+		fmt.Printf("ошибка чтения файла: %s", err.Error())
+		return err
+	}
+
+	// десериализуем JSON в YAML
+	if err = json.Unmarshal(jsonFile, &yamlData); err != nil {
+		fmt.Printf("ошибка при десериализации: %s", err.Error())
+		return err
+	}
+
+	// сериализуем YAML
+	yamlBytes, err := yaml.Marshal(&yamlData)
+	if err != nil {
+		fmt.Printf("ошибка при сериализации yaml: %s", err.Error())
+		return err
+	}
+
+	// записываем слайс байт в файл
+	_, err = f.Write(yamlBytes)
+	if err != nil {
+		fmt.Printf("ошибка при записи данных в файл: %s", err.Error())
+		return err
+	}
 
 	return nil
 }
 
 // Encoding перекодирует файл из YAML в JSON
 func (y *YAMLData) Encoding() error {
-	// Ниже реализуйте метод
-	// ...
+	var jsonData JSONData
+
+	// читаем файл YAML
+	yamlFile, err := os.ReadFile(y.FileInput)
+	if err != nil {
+		fmt.Printf("ошибка чтения файла: %s", err.Error())
+		return err
+	}
+
+	// десериализуем YAML в JSON
+	if err = yaml.Unmarshal(yamlFile, &jsonData); err != nil {
+		fmt.Printf("ошибка при десериализации: %s", err.Error())
+		return err
+	}
 
 	return nil
 }
